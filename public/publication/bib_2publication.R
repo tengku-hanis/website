453a31787aaac08b9dcb7a179276cefcd9534f6e
tengku-hanis <tengkuhanismokhtar@gmail.com>
2021-11-05 03:54:32 +0800

    rebuild site

---
+++ public/publication/bib_2publication.R
@@ -0,0 +1,13 @@
+# Convert bibtext to academic theme for website
+
+# Source function
+source("https://raw.githubusercontent.com/tengku-hanis/bibtex-academic/main/bibtex_2academic.R")
+
+# Bibtex file in my directory
+my_bibfile <- "C:/Tengku/Sync_PC_Laptop/Jabref/mypublication.bib"
+
+# Where I want the markdown output to go
+outfold <- "content/publication"
+
+# Use the conversion function
+bibtex_2academic(my_bibfile, outfold, abstract = F, overwrite = F)
